43b9a6ee80710c2e07a6590d0362df1d8616696b
Pratik Joshi <pratikjo@usc.edu>
2021-02-13 17:08:27 -0500

    Added a POST method to the class

--- src/main/java/com/example/helloworld/resources/HelloWorldResource.java
+++ src/main/java/com/example/helloworld/resources/HelloWorldResource.java
@@ -4,12 +4,14 @@ import com.example.helloworld.api.Saying;
 import com.codahale.metrics.annotation.Timed;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.Optional;
+import javax.ws.rs.core.Response;
 
 /*
 * A Dropwizard application can contain many resource classes, each corresponding to its own URI pattern.
@@ -35,5 +37,15 @@ public class HelloWorldResource {
     public Saying sayHello(@QueryParam("name") Optional<String> name) {
         final String value = String.format(template, name.orElse(defaultName));
         return new Saying(counter.incrementAndGet(), value);
+    }
+
+    @POST
+    public Response postValueToDB(String input) {
+        if (input == null || input.length() == 0)
+            return Response.status(Response.Status.NO_CONTENT).build();
+
+        System.out.println("Got the input in the POST method as : " + (input));
+
+        return Response.status(Response.Status.ACCEPTED).build();
     }
 }
